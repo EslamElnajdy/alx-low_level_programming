e9275031b0bfd1b03fabec5d949ee7f1eeffb0f0
Eslam Mohamed <eslammhamed425@gmail.com>
2023-03-17 06:24:01 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include<stdio.h>
+/**
+ * the main funcation prints the pharse "Programming is like building a multilingual puzzle
+ * return 0 if done ,non-zero otherwise
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
