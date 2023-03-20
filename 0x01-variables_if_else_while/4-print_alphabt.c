56536cb438347c326b57e34276beeae6dd262df1
Eslam Mohamed <eslammhamed425@gmail.com>
2023-03-20 05:55:16 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print all letter accept q and e,
+ * following a newline
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++){
+		if (ch != q || ch != e)
+			continue;
+		putchar(ch);
+	}
+	putchar('\n');
+}
