41bfac87466d0b15fe9bbed947efa9d826493200
Eslam Mohamed <eslammhamed425@gmail.com>
2023-03-23 09:00:05 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,8 @@
 #include "main.h"
+
 /**
- * print_line - draws a straight line
- * @n: the number of times the character _ should 
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character _ should be printed
  */
 void print_line(int n)
 {
