bb26ffdd4afd917c42895f0ce75fadf6a585014b
Eslam Mohamed <eslammhamed425@gmail.com>
2023-03-31 16:14:44 -0700

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * reverse_array - Write a function that reverses the content of an array of
  * integers.
@@ -13,9 +12,14 @@ void reverse_array(int *a, int n)
 	int i, last;
 
 	last = n - 1;
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n / 2; i++)
 	{
-		a[i] = a[last];
+		int start, end;
+
+		start = a[i];
+		end = a[last];
+		a[i] = end;
+		a[last] = start;
 		last--;
 	}
 }
