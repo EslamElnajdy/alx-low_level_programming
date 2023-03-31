8abdf23d1bbbc14a70148e6f5f614123481d7c0b
Eslam Mohamed <eslammhamed425@gmail.com>
2023-03-31 14:22:31 -0700

    update

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,18 +10,14 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i,j;
+	int i, j;
 
-	i = 0;
-	j = 0;
-
-	while (dest[i] != '\0')
-		i++;
-	while(src[j] != '\0')
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; src[j] != '\0'; j++)
 	{
 		dest[i] = src[j];
 		i++;
-		j++;
 	}
 	return (dest);
 }
