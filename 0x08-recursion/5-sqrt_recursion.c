073edd0a78b35377d121e2585e61d98ef79b9cdc
Eslam Mohamed <eslammhamed425@gmail.com>
2023-04-06 16:34:47 -0700

    task

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - return the square root
+ * @n: number
+ * Return: the square root
+ */
+int _sqrt_recursion(int n)
+{
+	int a = n ** (1/2);
+
+	if (a * a = n)
+		return (a);
+	return (-1);
+}
