32a35f4774f0d203d1c31547d7e1df47da68c155
Eslam Mohamed <eslammhamed425@gmail.com>
2023-04-07 05:14:06 -0700

    task

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * len - obtains length of a
+ * @a: string
+ * @b: integer to count length
+ * Return: On success 1.
+ */
+int len(char *a, int b)
+{
+	if (*a == 0)
+		return (l + 1);
+	return (len(a + 1, b + 1));
+}
+/**
+ * com - compares string vs string reverse
+ * @n: string
+ * @k: length
+ * Return: On success 1.
+ */
+
+int com(char *n, int k)
+{
+	if (*n != *(n + k)
+		return (0);
+	if (*n == 0)
+		return (1);
+	return (com(a + 1,l - 2));
+}
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to evaluate
+ * Return: On success 1.
+ */
+int is_palindrome(char *s)
+{
+	int m;
+
+	m = len(s, 0);
+	return (com(s, m));
+}
