dd010c113ca4ca8846bfffd89ed35527a83ad146
Eslam Mohamed <eslammhamed425@gmail.com>
2023-04-13 17:34:02 -0700

    task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * string_nconcat -  returned pointer shall point to a newly
+ * allocated space in memory
+ * @s1: string1
+ * @s2: string2
+ * @n: number of bytes of s2
+ * Return:
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p;
+	unsigned int i, j;
+
+
+	if (s1 == NULL)
+		i = 0;
+	if (s1)
+	{
+		for (i = 0; s1[i]; i++)
+			;
+	}
+	if (s2 == NULL)
+		n = 0;
+	p = malloc(sizeof(char) * (i + n + 1));
+	if (p == NULL)
+		return (NULL);
+	for (j = 0; j < i; j++)
+		p[j] = s1[j];
+	for (j = 0; j < n; j++)
+		p[j + i] = s2[j];
+	return (p);
+}
