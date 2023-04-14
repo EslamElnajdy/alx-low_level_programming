ba6b9aaf64b01221871a0cd4fd501b9a31805536
Eslam Mohamed <eslammhamed425@gmail.com>
2023-04-14 09:03:41 -0700

    task 2

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib>
+/**
+ * array_range - function
+ * @min: input
+ * @max: input
+ * Return: address
+ */
+int *array_range(int min, int max)
+{
+	int i, *p;
+	
+	i = 0;
+
+	if (min > max)
+		return (NULL);
+	p = malloc((max - min + 1) * sizeof(int));
+	if (p == NULL)
+		return (NULL);
+	while(min <= max)
+	{
+		p[i] = min;
+		min++;
+		i++;
+	}
+	return (p);
+}
