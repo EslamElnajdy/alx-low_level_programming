9a9271b55d17b6f56ccbd7c5b7f9fecc7a1a3c24
Eslam Mohamed <eslammhamed425@gmail.com>
2023-04-17 17:36:33 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog attributes
+ * @name: input
+ * @age: input
+ * @owner: the owner
+ * Description: this a data about dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
