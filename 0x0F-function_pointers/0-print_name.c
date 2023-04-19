9869090a08e91fdb01059c8bfc1e333a8cc28b00
Eslam Mohamed <eslammhamed425@gmail.com>
2023-04-19 16:34:03 -0700

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include "function_pointers.h"
+
 
 /**
  * print_name - to print name
@@ -9,5 +10,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	(*f)(name);
 }
