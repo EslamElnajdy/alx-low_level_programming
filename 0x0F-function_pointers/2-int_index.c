aa77f3ab26d75bcf7f8393864bdd82a2814e3e6e
Eslam Mohamed <eslammhamed425@gmail.com>
2023-04-19 17:22:44 -0700

    task 0

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,6 +1,7 @@
 #include "function_pointers.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * int_index - return index place if comparison = true, else -1
  * @array: array
@@ -8,7 +9,6 @@
  * @cmp: pointer to func of one of the 3 in main
  * Return: 0
  */
-
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
