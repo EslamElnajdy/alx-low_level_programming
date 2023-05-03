a02bbd0d3de57b7f82902a3aa03c9eedabefb633
Eslam Mohamed <eslammhamed425@gmail.com>
2023-05-03 11:12:44 -0700

    initial commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,10 +6,12 @@
  * print_listint - Write a function that prints all 
  * the elements of a listint_t list.
  * @h: head. 
+ * Return: number of nodes.
 */
 size_t print_listint(const listint_t *h)
 {
     size_t n = 0;
+
     while (h)
     {
         printf("%d\n",h->n);
