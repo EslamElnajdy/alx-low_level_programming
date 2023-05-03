46e9f8364c8d71e4b12d1c171a2891bde80fcc11
Eslam Mohamed <eslammhamed425@gmail.com>
2023-05-03 12:11:19 -0700

    task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,12 +8,13 @@
 
 size_t listint_len(const listint_t *h)
 {
-    size_t n = 0;
+ 
+	size_t n = 0;
 
-    while (h)
-    {
-        n++;
-        h = h->next;
-    }
-    return (n);
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
 }
