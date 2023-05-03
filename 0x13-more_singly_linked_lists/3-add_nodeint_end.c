068cc4197e6375165b7bc7bee5b216a5b6f7e7a2
Eslam Mohamed <eslammhamed425@gmail.com>
2023-05-03 14:37:40 -0700

    task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -6,27 +6,26 @@
  * @head: pointer to the first element of the list.
  * @n: int to set in the new node.
  * Return: address of the new element, or NULL if it failed
- **/
+ */
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-    listint_t *new, *temp = *head;
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-        return (NULL);
-    new->n = n;
-    new->next = NULL;
-    if (temp)
-    {
-        while (temp->next)
-        {
-            temp = temp->next;
-        }
-        temp->next = new;
-    }
-    else
-    {
-        *head = new;
-    }
-    return (new);
+	listint_t *new, *aux = *head;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+
+	if (aux)
+	{
+		while (aux->next)
+			aux = aux->next;
+		aux->next = new;
+	}
+	else
+		*head = new;
+
+	return (new);
 }
