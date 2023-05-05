d0185cef1b664a170345440e67a788c3627404d0
Eslam Mohamed <eslammhamed425@gmail.com>
2023-05-04 18:07:49 -0700

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <unistd.h>
+#include <limits.h>
 /**
  * print_binary - print an unsigned int in binary
  *
@@ -7,17 +8,24 @@
  *
  * Return: void
  */
-
 void print_binary(unsigned long int n)
 {
-    while (n > 0)
-    {
-        putchar((n & 1) ? '1' : '0');
-        n >>= 1;
-    }
-    if (n == 0)
-        putchar('0');
-    if (n > 1) {
-        print_binary(n);
-    }
+	unsigned long int printbit = 1ul << 63;
+	char c = '0';
+
+	while (!(printbit & n) && printbit != 0)
+		printbit = printbit >> 1;
+
+	if (printbit == 0)
+		write(1, &c, 1);
+
+	while (printbit)
+	{
+		if (printbit & n)
+			c = '1';
+		else
+			c = '0';
+		write(1, &c, 1);
+		printbit = printbit >> 1;
+	}
 }
