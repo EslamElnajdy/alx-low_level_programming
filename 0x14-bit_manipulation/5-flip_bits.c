03c1854c5d32c249e7d4a8cadc548cda8862c5d6
Eslam Mohamed <eslammhamed425@gmail.com>
2023-05-05 18:04:39 -0700

    task

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -4,13 +4,13 @@
  * need to flip to get from one number to another
  * @n: number one.
  * @m: number two.
- *
  * Return: number of bits.
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int res = n ^ m;
 	int count = 0;
+
 	while (res)
 	{
 		if (res & 1)
