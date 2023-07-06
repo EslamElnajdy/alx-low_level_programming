bf408da3b24ba9226a9213b18b57bdf835b0e9fa
Eslam Mohamed <eslammhamed425@gmail.com>
2023-07-06 10:30:34 -0700

    fix errors

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,9 +10,13 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
-	int len = 0;
-	int base = 1;
+	unsigned int num;
+	int len;
+	int base;
+
+	num = 0;
+	len = 0;
+	base = 1;
 
 	if (b == NULL)
 	{
