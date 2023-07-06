a3f20a8f1be362de8bb6fa86144d09f722b800a0
Eslam Mohamed <eslammhamed425@gmail.com>
2023-07-06 11:15:32 -0700

    fix errors

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,35 +3,35 @@
 /**
  * binary_to_uint - converts a binary number to an
  * unsigned int.
- * @b: binary string.
+ * @b: binary.
  *
  * Return: unsigned int.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int unint;
-	int len, base;
-	
+	unsigned int ui;
+	int len, base_two;
 
 	if (!b)
 		return (0);
-	unint = 0;
+
+	ui = 0;
 
 	for (len = 0; b[len] != '\0'; len++)
 		;
 
-	for (len--, base = 1; len >= 0; len--, base *= 2)
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
 		if (b[len] != '0' && b[len] != '1')
 		{
 			return (0);
 		}
 
-		if (b[len] == '1')
+		if (b[len] & 1)
 		{
-			unint += base;
+			ui += base_two;
 		}
 	}
 
-	return (unint);
+	return (ui);
 }
