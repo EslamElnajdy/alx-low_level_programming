58541b7bcf9c65efabbb350c6b480b54ea350f21
Eslam Mohamed <eslammhamed425@gmail.com>
2023-07-06 17:46:22 -0700

    initial commit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,22 +1,23 @@
 #include "main.h"
 
 /**
- * get_bit - returns the value of a bit at a given index.
+ * get_bit - return the value
  * @n: number
- * @index: index
-Returns: the value of the bit at index index or -1 if an error occured
-*/
+ * @index: number
+ * Return: value of index
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
-    int rem;
-    int arr[31];
-    int i = 0;
-    while (n)
-    {
-        rem = n % 2;
-        n = n / 2;
-        arr[i] = rem; 
-        i++;
-    }
-    return (arr[index]);
+	unsigned int rem = 0;
+	int arr[31];
+	int i = 0;
+
+	while (n)
+	{
+		rem = n % 2;
+		n = n / 2;
+		arr[i] = rem;
+		i++;
+	}
+	return (arr[index]);
 }
