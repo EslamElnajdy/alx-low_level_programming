45506258d9527425027c112af4718fc9ba7bbabb
Eslam Mohamed <eslammhamed425@gmail.com>
2023-07-11 02:01:54 +0300

    do commit

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * append_text_to_file - append
+ * @filename: file
+ * @text_content: text
+ * Return: 1 ok, -1 otherwise
+*/
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int file;
