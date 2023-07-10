627e00e044c415a17dbe83f1e95d5882ebfd9e7f
Eslam Mohamed <eslammhamed425@gmail.com>
2023-07-11 01:35:33 +0300

    initial commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,7 +2,7 @@
 /**
  * create_file - creates a file.
  * @filename: name of the file to create.
- * @text_content: is a NULL terminated string to write to the file.
+ * @text_content: is a NULL terminated
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
