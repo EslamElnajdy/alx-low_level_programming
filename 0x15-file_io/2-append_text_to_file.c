ecf40cdcf6f011c36aba5405e39dacf45f5549e8
Eslam Mohamed <eslammhamed425@gmail.com>
2023-07-11 02:06:15 +0300

    fix error

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,16 +7,21 @@
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file;
+	int file, i = 0;
 
 	if (!filename)
 		return (-1);
-	if (!text_content)
-	return (-1);
+	if (text_content == NULL)
+		text_content = "";
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
+
 	file = open(filename, O_RDWR | O_APPEND);
 	if (!file)
 	return (-1);
-	file = write(file, text_content, strlen(text_content));
+	file = write(file, text_content, i);
 	if (!file)
 		return (-1);
 	close(file);
