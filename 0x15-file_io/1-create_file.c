571250bfc76f702dffc2fcdb6c0778a15d70b3df
Eslam Mohamed <eslammhamed425@gmail.com>
2023-07-11 01:32:03 +0300

    initial commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,12 +8,11 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-    int i = 0;
+	int i = 0;
 
 	if (filename == NULL)
 		return (-1);
-
-    while (text_content[i] != '\0')
+	while (text_content[i] != '\0')
 	{
 		i++;
 	}
