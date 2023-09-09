01b490c270345ef72e9b6a573e81c4520ebba3ea
Eslam Mohamed <eslammhamed425@gmail.com>
2023-09-09 03:55:27 +0300

    task 2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives you the index of a key.
+ * @key: the string key.
+ * @size: the size of array.
+ * 
+ * Return: the index of of the key.
+*/
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+    return (hash_djb2(key) % size);
+}
