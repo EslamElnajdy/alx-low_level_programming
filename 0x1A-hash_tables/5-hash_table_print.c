c9fb9ff2fd20c9157e85eff60f7214ffb7baf778
Eslam Mohamed <eslammhamed425@gmail.com>
2023-09-11 02:52:10 +0300

    task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table.
+ * @ht: the hash table.
+*/
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned int i = 0;
+	int comma = 0;
+
+	if (!ht)
+		printf("{}\n");
+	else
+	{
+		printf("{");
+		for (i = 0; i < ht->size; i++)
+		{
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				if (comma)
+					printf(", ");
+				printf("'%s'", node->key);
+				printf(": ");
+				printf("'%s'", node->value);
+				node = node->next;
+				comma = 1;
+			}
+		}
+		printf("}\n");
+	}
+}
